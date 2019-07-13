fbd69a65f8aa420e93f5af863be0ed2f77f49a4a
lpzilyav <1449110081@qq.com>
2019-07-13 10:52:09 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,18 @@
+package com.atguigu.bean;
+
+import java.util.Date;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld !");
+
+        Date date = new Date();
+        System.out.println(date);
+
+    }
+
+    public void test(){
+        System.out.println("TEST");//这是一个注释
+
+    }
+}
---
+++ src/com/atguigu/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atguigu.java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.main");
+        System.out.println("HelloWorld");
+        String a ="dddd";
+        if (a != null) {
+            System.out.println(a);
+        }
+    }
+}
